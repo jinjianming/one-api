815eba2ecf572e9f64da1a51dff5b82304ac930c
jinjianmingming <17610798380@163.com>
2024-05-16 10:10:38 +0800

    修复地址获取逻辑,修改回复中文

--- web/berry/src/views/Chat/index.js
+++ web/berry/src/views/Chat/index.js
@@ -7,8 +7,18 @@ const Chat = () => {
         // const [tokens, setTokens] = useState([]);
 
         const res = await API.get(`/api/token/`);
+        let status = localStorage.getItem('status');
+        const chatLink = localStorage.getItem('chat_link');
+        let serverAddress = '';
+        if (status) {
+            status = JSON.parse(status);
+            serverAddress = status.server_address;
+        }
+        if (serverAddress === '') {
+            serverAddress = window.location.origin;
+        }
         const key = res.data.data[0].key
-        const url = `https://like.chatapi.asia/#/?settings={"key":"sk-${key}","url":"https://chat.chatapi.asia"}`;
+        const url = `${chatLink}/#/?settings={"key":"sk-${key}","url":${serverAddress}`;
         window.open(url)
 
 
