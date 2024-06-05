9846b22daa356f0366e8056f044a9b5a3c60f27e
jinjianmingming <17610798380@163.com>
2024-06-05 15:39:26 +0800

    修复打开问题

--- web/berry/src/views/Chat/index.js
+++ web/berry/src/views/Chat/index.js
@@ -2,31 +2,43 @@ import React, { useEffect, useState } from "react";
 import { API } from "../../utils/api";
 import "./index.css";
 
+const useIsSmallScreen = () => {
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return isSmallScreen;
+};
+
 const Chat = () => {
     const [chatUrl, setChatUrl] = useState("");
     const [loading, setLoading] = useState(true);
+    // const isSmallScreen = useIsSmallScreen();
 
     const loadTokens = async () => {
         try {
             const res = await API.get(`/api/token/`);
             const siteInfo = JSON.parse(localStorage.getItem('siteInfo'));
-
             if (!siteInfo) {
                 console.error("siteInfo not found in localStorage");
                 setLoading(false);
                 return;
             }
-
+            // const url = `https://like.chatapi.asia/#/?settings={"key":"sk-xxx","url":"https://chat.chatapi.asia"}`;
+            const serverAddress = siteInfo.server_address;
             const key = res.data.data[0].key;
-            const settings = {
-                keyVaults: {
-                    openai: {
-                        apiKey: key,
-                        baseURL: 'https://like2.chatapi.asia/v1',
-                    },
-                },
-            };
-            const url = `/?settings=${JSON.stringify(settings)}`;
+            // const url = `${siteInfo.chat_link}/#/?settings={"key":"sk-${key}","url":"${serverAddress}"}`;
+            const url = `https://like2.chatapi.asia/?settings={"keyVaults":{"openai":{"apiKey":"sk-${key}","baseURL":"${serverAddress}/v1"}}}`;
 
             setChatUrl(url);
         } catch (error) {
